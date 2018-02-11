01aa5422bad491984cbe8eff1377c0e416a86b88
Steve Bishop <xipooo@gmail.com>
2018-02-10 19:59:03 -0800

    Step 2 Example

--- WozUSpringDemo/src/main/java/com/wozu/springdemo/HelloWorldController.java
+++ WozUSpringDemo/src/main/java/com/wozu/springdemo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.wozu.springdemo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloWorldController {
 	@RequestMapping("/greeting")
 	public String getGreeting() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping(value="/greeting", method=RequestMethod.POST)
+	public String postGreeting() {
+		return "Hello Post World!";
 	}
 }
