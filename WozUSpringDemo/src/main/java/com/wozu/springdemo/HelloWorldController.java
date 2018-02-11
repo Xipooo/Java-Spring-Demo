07244cb41cf7f590fcd714532af9fbb80bc68fbc
Steve Bishop <xipooo@gmail.com>
2018-02-11 15:48:58 -0800

    Added comments

--- WozUSpringDemo/src/main/java/com/wozu/springdemo/HelloWorldController.java
+++ WozUSpringDemo/src/main/java/com/wozu/springdemo/HelloWorldController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloWorldController {
 	
+	//Uses GET method
 	@RequestMapping("/greeting")
 	public String getGreeting() {
 		return "Hello World!";
 	}
 	
+	//Uses POST method
 	@RequestMapping(value="/greeting", method=RequestMethod.POST)
 	public String postGreeting() {
 		return "Hello Post World!";
