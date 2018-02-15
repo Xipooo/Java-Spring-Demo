a85fda3ba5497a2cbb326ef707d03f3016529528
Steve Bishop <xipooo@gmail.com>
2018-02-14 22:14:45 -0800

    Added Thymeleaf Templating

--- WozUSpringDemo/src/main/java/com/wozu/springdemo/MvcConfiguration.java
+++ WozUSpringDemo/src/main/java/com/wozu/springdemo/MvcConfiguration.java
@@ -5,25 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-// Add View Resolver class for resolving views
 @Configuration
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter{
-    @Bean
-    public ViewResolver getViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".html");
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
-    }
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+	// Add View Controllers that bypass a need for Controller classes
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		super.addViewControllers(registry);
+		registry.addViewController("/security").setViewName("security");
+		registry.addViewController("/members").setViewName("members");
+	}
 
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }    
+//	With Thymeleaf installed views must be in src/main/resources/templates
+//	@Bean
+//	public ViewResolver getViewResolver() {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		resolver.setPrefix("/WEB-INF/views/");
+//		resolver.setSuffix(".html");
+//		resolver.setExposeContextBeansAsAttributes(true);
+//		return resolver;
+//	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
