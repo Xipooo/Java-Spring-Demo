81f1afc9f3b5e1bddf94e17dcaa48ecc8741d2e9
Steve Bishop <xipooo@gmail.com>
2018-02-21 19:21:33 -0800

    H2 Database and more HTTP Actions

--- WozUSpringDemo/src/main/java/com/wozu/springdemo/PersonController.java
+++ WozUSpringDemo/src/main/java/com/wozu/springdemo/PersonController.java
@@ -2,6 +2,7 @@ package com.wozu.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class PersonController {
 	
 	// Create Person method
 	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public Person addPerson(Person person) {
+	public Person addPerson(@RequestBody Person person) {
 		return personRepository.save(person);
 	}
 	
@@ -29,5 +30,27 @@ public class PersonController {
 	@RequestMapping(value="/{id}")
 	public Person getPersonById(@PathVariable Long id) {
 		return personRepository.findOne(id);
+	}
+	
+	// Read All Persons method
+	@RequestMapping(value="/")
+	public Iterable<Person> getAllPeople() {
+		return personRepository.findAll();
+	}
+	
+	// Update Person method
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public Person updatePersonById(@PathVariable Long id, @RequestBody Person updatePerson) {
+		Person person = personRepository.findOne(id);
+		person.setFirstName(updatePerson.getFirstName());
+		person.setLastName(updatePerson.getLastName());
+		person.setEmail(updatePerson.getEmail());
+		return personRepository.save(person);
+	}
+	
+	// Delete Person method
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public void deletePersonById(@PathVariable Long id) {
+		personRepository.delete(id);
 	}
 }
