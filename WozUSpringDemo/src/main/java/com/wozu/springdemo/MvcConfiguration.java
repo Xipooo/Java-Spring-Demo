40c3212912c1ee7bbc5e6378af5f5696f3b2aa5a
Steve Bishop <xipooo@gmail.com>
2018-02-21 19:45:25 -0800

    Removed mysql references and fixed person controller

--- WozUSpringDemo/src/main/java/com/wozu/springdemo/MvcConfiguration.java
+++ WozUSpringDemo/src/main/java/com/wozu/springdemo/MvcConfiguration.java
@@ -1,13 +1,10 @@
 package com.wozu.springdemo;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
